1a94d169335381b210409c93bb2b6a2abed9a797
Faiznasrulyf <148850783+Faiznasrulyf@users.noreply.github.com>
2023-10-24 14:44:34 +0700

    Update contact.jsx

---
+++ src/Page/contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ContactPage() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      {
+        faizyunan656@gmail.com
+      }
+    </div>
+  );
+}
+
+export default ContactPage;
